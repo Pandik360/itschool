e70fa152f0f88e9e5b4dd7295ee3af78e36beb7e
wwwvl <wwwvlad36025585@mail.ru>
2020-02-21 12:29:02 +0300

    aga

--- app/src/main/java/com/example/programmm/Main2Activity.java
+++ app/src/main/java/com/example/programmm/Main2Activity.java
@@ -2,10 +2,13 @@ package com.example.programmm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.*;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -13,6 +16,30 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        final CheckBox ch1 = findViewById(R.id.checkBox);
+        final CheckBox ch2 = findViewById(R.id.checkBox2);
+        final CheckBox ch3 = findViewById(R.id.checkBox3);
+        final BluetoothAdapter bl= BluetoothAdapter.getDefaultAdapter();
+        Button cb = findViewById(R.id.button7);
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ch1.isChecked())
+                {
+                    if (!bl.enable())
+                        Toast.makeText(getApplicationContext(),"Включение", Toast.LENGTH_SHORT);
+                    else
+                        Toast.makeText(getApplicationContext(),"Bluetooth включён", Toast.LENGTH_LONG);
+                    bl.enable();
+                }
+                if(ch2.isChecked())
+                    Toast.makeText(getApplicationContext(),"Подключено",Toast.LENGTH_SHORT);
+                if(ch3.isChecked())
+                    bl.disable();
+                    if(bl.disable())
+                    Toast.makeText(getApplicationContext(),"Всё выключено(пока что только bluetooth)",Toast.LENGTH_LONG);
+            }
+        });
         Button but = findViewById(R.id.button2);
         but.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/programmm/MainActivity.java
+++ app/src/main/java/com/example/programmm/MainActivity.java
@@ -6,11 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.bluetooth.*;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int REQUEST_ENABLE_BT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,48 +17,10 @@ public class MainActivity extends AppCompatActivity {
         but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                //startActivity(intent);
-                BluetoothAdapter bl= BluetoothAdapter.getDefaultAdapter();
-               if (!bl.isEnabled()) {
-                   bl.enable();
-//                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                 startActivity(intent);
-               }
-               else
-                {
                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                    startActivity(intent);
-                }
 
-
-
-//               if(bl==null)
-//                {
-//                   Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-//                  startActivity(intent);
-//              }
             }
         });
     }
 }
-
-
-
-
-
-//    public static boolean setBluetooth(boolean enable)
-//    {
-//        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-//        boolean isEnabled = bluetoothAdapter.isEnabled();
-//        if (enable && !isEnabled)
-//        {
-//            return bluetoothAdapter.enable();
-//        }
-//        else if(!enable && isEnabled)
-//        {
-//            return bluetoothAdapter.disable();
-//        }
-//}
